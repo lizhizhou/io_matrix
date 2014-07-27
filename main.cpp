253f2fe1536f7d7e89332e0713a69997bdced986
lizhizhou <lzz@meteroi.com>
2014-07-27 02:49:33 -0700

    initial version

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 27, 2014
+ *      Author: zhizhouli
+ */
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+class device
+{
+	string name;
+};
+
+class io
+{
+	string name;
+};
+
+int main()
+{
+	cout << "lizhizhou";
+	return 0;
+}
+
+
+
