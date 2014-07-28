298814211f3f891bdbc435acd42078478b0d9370
lizhizhou <lzz@meteroi.com>
2014-07-28 00:43:15 -0700

    Signed-off-by: lizhizhou <lzz@meteroi.com>

--- main.cpp
+++ main.cpp
@@ -17,17 +17,44 @@ enum io_direction {
 
 class io
 {
+private:
 	string name;
 	io_direction directory;
+	int width;
+public:
+	string to_verilog_name();
+	string to_c_name();
 };
 
-class device
+string io::to_verilog_name()
 {
+	return name;
+}
+
+string io::to_c_name()
+{
+	return name;
+}
+
+class interface
+{
+private:
 	string name;
 	list<io> io_pin_list;
+public:
+	string to_verilog_name();
+	string to_c_name();
 };
 
+string interface::to_verilog_name()
+{
+	return name;
+}
 
+string interface::to_c_name()
+{
+	return name;
+}
 
 int main()
 {
