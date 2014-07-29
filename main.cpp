2159b4efb3454ac66feaab46169c2c87b4f41bcc
lizhizhou <lzz@meteroi.com>
2014-07-29 01:10:56 -0700

    add class direction

--- main.cpp
+++ main.cpp
@@ -9,18 +9,52 @@
 #include <list>
 using namespace std;
 
-enum io_direction {
-	INPUT,
-	OUTPUT,
-	INOUT
+class direction {
+private:
+	static const int INPUT  = 1;
+	static const int OUTPUT = 2;
+	static const int INOUT  = 3;
+	int direction;
+public:
+	string to_string();
+	void input();
+	void output();
+	void inout();
 };
+
+string direction::to_string()
+{
+	switch(direction) {
+	case 1:
+		return "input";
+	case 2:
+		return "ouput";
+	case 3:
+		return "inout";
+	default:
+		return "undefine";
+	}
+}
+void direction::input()
+{
+	direction = INPUT;
+}
+void direction::output()
+{
+	direction = OUTPUT;
+}
+void direction::inout()
+{
+	direction = INOUT;
+}
 
 class io
 {
 private:
 	string name;
-	io_direction directory;
+	direction directory;
 	int width;
+	class io* connector;
 public:
 	string to_verilog_name();
 	string to_c_name();
